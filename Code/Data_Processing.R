ea4f4b0e6b3cda48fac407672bf57d1140e4471b
Rach <rlb18@ic.ac.uk>
2020-05-26 19:40:49 +0100

    Added functions to get more generic versions of the threat index
    Uses the highest level groupings of the IUCN threat data

--- Code/Data_Processing.R
+++ Code/Data_Processing.R
@@ -1,6 +1,8 @@
 require(dplyr)
 require(tidyverse)
 
+`%!in%` = Negate(`%in%`)
+
 Species_History <- read.csv("../Data/Species_History050220.csv")
 Species_Data <- read.csv("../Data/Species_Data.csv", stringsAsFactors = F)
 #setwd("~/Documents/PhD/MiniProject/PhDMiniProjWorkflow/Code")
@@ -252,19 +254,64 @@ Final_Species <- unique(Corrected_cats$taxonid)
 Species_Threats <- read.csv("../Data/Species_Threats_7.csv", header = T, stringsAsFactors = F)
 Species_Threats <- Species_Threats %>% rename(taxonid = id)
 
+Assign_threats <- function(Species_Threats, Threats_cat){
+  Threat <- NA
+  if (Species_Threats["Threat_mid"] %in% Threats_cat[,"Codes"]){
+    Threat <- as.character(Threats_cat[which(Threats_cat[,"Codes"] %in% Species_Threats["Threat_mid"]), "Threats"])
+  }
+  return(Threat)
+}
+
+
 # Func to process the threat data into main threat types
 Process_Threats <- function(Species_Threats, Final_Species){
   # Filter the threat data down to species that are included in the modelling
   Species_Threats <- dplyr::filter(Species_Threats, taxonid %in% Final_Species)
   # Put the highest level of threat in a new column
-  Species_Threats <- Species_Threats %>% mutate(Threat_broad = str_extract(result.code, "([0-9]+)(?=\\.)"))
-  # Recode them according to my cateogries
-  Species_Threats$Threat_broad <- recode(Species_Threats$Threat_broad, "1" = "HabitatChange", "2" = "HabitatChange", "3" = "HabitatChange", "4" = "HabitatChange", "5" = "Overexploitation", "6" = "HabitatChange", "7" = "HabitatChange", "8" = "Invasives", "9" = "Pollution", "10" = "Natural", "11" = "ClimateChange", "12" = "Other")
-  Species_Threats$Threat_broad[which(Species_Threats$result.code=="5.3.3"|Species_Threats$result.code=="5.3.4")] <- "HabitatChange"
+  #Species_Threats <- Species_Threats %>% mutate(Threat_broad = str_extract(result.code, "([0-9]+)(?=\\.)"))
+  # Does the same for mid-level
+  Species_Threats <- Species_Threats %>% mutate(Threat_mid = str_extract(result.code, "([0-9]+\\.[0-9]+)"))
+  # Make a reference df of my categories and their codes
+  Threats_cat <- data.frame(Threats = c("Agriculture", "Agriculture", "Agriculture", "Hunting", "Logging", "Fishing", "Invasives", "Climate", "Climate", "Climate", "Climate", "Climate"), Codes = c(2.1,2.2,2.3,5.1,5.3,5.4,8.1,11.1,11.2,11.3,11.4,11.5))
+  # Assign those cats
+  Threats_cat$Codes <- as.numeric(Threats_cat$Codes)
+  Species_Threats$Threat_mid <- as.numeric(Species_Threats$Threat_mid)
+  for (i in 1:nrow(Species_Threats)){
+    Species_Threats$Threat_broad[i]<-Assign_threats(Species_Threats[i,], Threats_cat)
+  }
   # Subset to only required columns
   Species_Threats <- Species_Threats[,c("taxonid", "Threat_broad")]
+  # Work out a list of species with NO threats listed
+  threatened_ids <- unique(Species_Threats$taxonid[is.na(Species_Threats$Threat_broad)==FALSE])
+  NA_IDs <- unique(Species_Threats$taxonid[Species_Threats$taxonid %!in% threatened_ids])
   # Make a reference vector of the threats
-  Threats <- c("HabitatChange", "Overexploitation", "Invasives", "Pollution", "ClimateChange", "Natural")
+  Threats <- unique(Species_Threats$Threat_broad[is.na(Species_Threats$Threat_broad)==FALSE])
+  # Get a list of all the taxon ids of the species for each threat
+  Threat_index <- lapply(Threats, function(i) unique(Species_Threats$taxonid[which(Species_Threats$Threat_broad==i)])) 
+  # rename to the actual threats for clarity
+  names(Threat_index) <- Threats
+  Threat_index[["NA"]] <- NA_IDs
+  return(Threat_index)
+}
+  
+Threat_index <- Process_Threats(Species_Threats, Final_Species)
+
+
+###### More generic version of above function, just sorts into base mid level cats.
+
+# Func to process the threat data into main threat types
+Process_Threats_generic <- function(Species_Threats, Final_Species){
+  # Filter the threat data down to species that are included in the modelling
+  Species_Threats <- dplyr::filter(Species_Threats, taxonid %in% Final_Species)
+  # Put the highest level of threat in a new column
+  # Does the same for mid-level
+  Species_Threats <- Species_Threats %>% mutate(Threat_broad = str_extract(result.code, "([0-9]+)(?=\\.)"))
+  # Subset to only required columns
+  Species_Threats <- Species_Threats[,c("taxonid", "Threat_broad")]
+  # Remove extra rows
+  Species_Threats <- unique(Species_Threats)
+  # Make a reference vector of the threats
+  Threats <- unique(Species_Threats$Threat_broad)
   # Get a list of all the taxon ids of the species for each threat
   Threat_index <- lapply(Threats, function(i) unique(Species_Threats$taxonid[which(Species_Threats$Threat_broad==i)])) 
   # rename to the actual threats for clarity
@@ -272,7 +319,7 @@ Process_Threats <- function(Species_Threats, Final_Species){
   return(Threat_index)
 }
 
-Threat_index <- Process_Threats(Species_Threats, Final_Species)
+Threat_index_generic <- Process_Threats_generic(Species_Threats, Final_Species)
 
 ## Use Species_Data to subset by taxon.
 Filter_taxa <-function(Species){
