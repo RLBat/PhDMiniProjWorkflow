7358a06d25979247e4deebc4502eecd57bd1634b
Ryan <rlb18@ic.ac.uk>
2021-10-19 18:41:03 +0100

    Added code to look at the binned data
    Binned across 4 years to help solve issues of spike years.

--- Code/sandbox.R
+++ Code/sandbox.R
@@ -25,6 +25,8 @@ p <- ggplot(data = dist, aes(x = Year, y = ratio, group = Category, colour = Cat
   theme_bw()
 p
 
+#########
+
 #check to see if any years have stupidly low assessments
 test<-unique(dist[,c("Year", "total")])
 
@@ -37,3 +39,21 @@ p <- ggplot(data = dist, aes(x = Year, y = ratio, group = Category, colour = Cat
 p
 
 ## much cleaner!  
+#########
+
+## Bin across multiple years
+
+year_ref<- data.frame(Year=unique(dist$Year), Ref = c(rep("94-97",3),rep("98-01",3),rep("02-05",4),rep("06-09",4),rep("10-13",4),rep("14-17",4),rep("18-19",2)))
+
+dist_bin<- merge(year_ref,dist)
+dist_bin$Ref <- factor(dist_bin$Ref, levels = c("94-97", "98-01", "02-05", "06-09", "10-13", "14-17", "18-19"))
+dist_bin <- dist_bin %>% group_by(Ref) %>% mutate("ratio"=(freq/sum(freq))) %>% mutate("total" = sum(freq))
+
+
+
+p <- ggplot(data = dist_bin, aes(x = Ref, y = ratio, fill=Category)) + 
+  geom_bar(position= "stack",stat="identity") + scale_fill_manual(values=c("lightblue", "darkcyan", "orange", "darkorange", "orangered3", "darkred")) +
+  theme_bw() + ylab("Proportion") + xlab("Year")
+p
+
+
