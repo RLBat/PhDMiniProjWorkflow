86313f8bd31940ea738ef474e4b817285a87dffa
Ryan <rlb18@ic.ac.uk>
2022-08-02 16:13:22 +0100

    Added bird body mass processing
    Also neatened the mammal processing and the file overall

--- Code/Body_Mass_Processing.R
+++ Code/Body_Mass_Processing.R
@@ -7,6 +7,64 @@ require(dplyr)
 Species <- read.csv("../Data/Corrected_SpeciesHistory_June222022.csv", header=T, stringsAsFactors = F)
 Historic_assess <- read.csv("../Data/Corrected_SpeciesHistory_June222022.csv", header = T, stringsAsFactors = F)
 
+# Grab the species with enough data to model post-cleaning
+Final_Species_List <- Historic_assess$scientific_name
+
+########### MAMMALS ######
+
+# List all mammals
+iucnmammals <- Species_Data[which(Species_Data$class_name == "MAMMALIA"),"scientific_name"]
+# Generate a list of all viable mammals
+viable_mammals <- iucnmammals[which(iucnmammals %in% Final_Species_List)]
+# import body mass data (which was manually taxon matched by Yuheng)
+mammal_bm <- read.csv("../Data/mammal_bodymass_2022.csv")
+# reduce body mass data down to viable species
+matched_mammal <- mammal_bm[which(mammal_bm$IUCN_name %in% viable_mammals),c(1,3,5)]
+
+# make a df with only the required species and add the body mass to each
+Mammal_bm_assessments <- Historic_assess[which(Historic_assess$scientific_name %in% matched_mammal$IUCN_name),]
+# rename cols for merge
+names(matched_mammal) <- c("taxonid", "scientific_name", "body_mass")
+# merge dfs to add body mass to the historic assessment data 
+Mammal_bm_assessments <- merge(Mammal_bm_assessments, matched_mammal)
+
+############ BIRDS ####
+
+# import bird body mass data
+birds_bm <- read.csv("../Data/Yuheng/viablebirds_mass_complete.csv")
+birds_bm <- birds_bm[,c(2:5)]
+
+# List all birds
+iucnbirds <- Species_Data[which(Species_Data$class_name == "AVES"),"scientific_name"]
+# Generate a list of all viable birds
+viable_birds <- iucnbirds[which(iucnbirds %in% Final_Species_List)]
+# reduce body mass data down to viable species
+matched_birds <- birds_bm[which(birds_bm$IUCN_name %in% viable_birds),c(1,3,4)]
+unmatched_birds <- birds_bm[which(birds_bm$IUCN_name %!in% viable_birds),c(1,3,4)]
+
+# make a df with only the required species and add the body mass to each
+bird_bm_assessments <- Historic_assess[which(Historic_assess$scientific_name %in% matched_birds$IUCN_name),]
+# rename cols for merge
+names(matched_birds) <- c("taxonid", "scientific_name", "body_mass")
+# merge dfs to add body mass to the historic assessment data 
+bird_bm_assessments <- merge(bird_bm_assessments, matched_birds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Final_Species_List <- Historic_assess$scientific_name
 
 ## read in a list of viable species to save processing time
