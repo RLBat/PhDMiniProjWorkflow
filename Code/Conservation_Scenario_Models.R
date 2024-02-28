43488d5c68e752c77789e7ec627a25cf95d9961e
Ryan <rlb18@ic.ac.uk>
2024-02-28 00:05:16 +0000

    Improved commenting and extended conservation modelling

--- Code/Conservation_Scenario_Models.R
+++ Code/Conservation_Scenario_Models.R
@@ -16,6 +16,13 @@ set.seed(333)
 #######################
 
 Categories = c("LC", "NT", "VU", "EN", "CR", "EX")
+props = as.numeric(c(LC = 18302, NT = 1966, VU = 2473, EN = 2429, CR = 1564, EX = 89))
+
+##############################
+## 2050 scenario modelling ##
+##############################
+
+## DATA ###
 
 # import transition tables
 NoEX_tt <- as.matrix(read.csv("../Data/NoExtinction_TransitionTables.csv", header = T)[,2:7])
@@ -25,10 +32,11 @@ NoRecover_tt <- as.matrix(read.csv("../Data/Norecovery_transitiontable.csv", hea
 Standard_tt <- as.matrix(read.csv("../Data/Standard_TransitionTables.csv", header = T)[,2:7])
 
 scenarios <- list(NoEX_tt, NoEXorTr_tt, NoTr_tt, NoRecover_tt, Standard_tt)
-
-
 scenario_names <- c("NoEX_tt", "NoEXorTr_tt", "NoTr_tt", "NoRecover_tt", "Standard_tt")
 
+## FUNCTIONS ##
+
+# generates transition table for a given number of years in the future
 # only works for a list of transition tables - list can be length = 1
 transitions_ot <- function(transition_tables = scenarios, time){
   scenario_tts <- list()
@@ -40,6 +48,7 @@ transitions_ot <- function(transition_tables = scenarios, time){
   return(scenario_tts)
 }
 
+# Predicts IUCN category composition for a list of transition tables at a given times
 predict_movements <- function(props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2429, CR = 1564, EX = 89), 
                               scenarios, time = 27){
   final_sums <- data.frame()
@@ -59,14 +68,22 @@ predict_movements <- function(props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2
   return(final_sums)
 }
 
+#grab the difference between the starting values and future values
+calc_diff <- function(future_tt, props){
+  future_tt[,1:6] = apply(future_tt[,1:6], 2, function(x) as.numeric(x))
+  future_tt[,1:6] <- t(apply(future_tt[,1:6], 1, function(x) x-props))
+  ## prep formatting for plotting
+  future_tt <- future_tt[,c(1:6,8)]
+  future_tt <- gather(future_tt, "category", "PEX", -scenario)
+  future_tt$category <- factor(future_tt$category, levels = c("LC", "NT", "VU", "EN", "CR", "EX"))
+  return(future_tt)
+}
+
+## WORKFLOW ##
+
 # generate 27 year predictions
 predict27 <- predict_movements(scenarios = list(NoEX_tt, NoEXorTr_tt, NoTr_tt, NoRecover_tt, Standard_tt), time = 27)
 predict27$scenario <- scenario_names
-
-# grab the original values
-props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2429, CR = 1564, EX = 89)
-props <- as.numeric(props)
-
 predict27_diff <- predict27
 
 #grab the difference between the starting values and after 27 years
@@ -75,11 +92,12 @@ predict27_diff[,1:6] <- t(apply(predict27_diff[,1:6], 1, function(x) x-props))
 
 ## plot those differences after 27 years
 
-#remove total row
+#remove total row and format for plotting
 predict27_diff <- predict27_diff[,c(1:6,8)]
 predict27_diff <- gather(predict27_diff, "category", "PEX", -scenario)
 predict27_diff$category <- factor(predict27_diff$category, levels = c("LC", "NT", "VU", "EN", "CR", "EX"))
 
+# generate the actual plot
 ggplot(data=predict27_diff, aes(x = scenario, y = PEX, fill = category)) +
   geom_bar(stat = "identity", position = "dodge") + 
   geom_abline(intercept = 0) +
@@ -89,13 +107,15 @@ ggplot(data=predict27_diff, aes(x = scenario, y = PEX, fill = category)) +
         axis.text.y = element_text(size=16) , axis.title = element_text(size=16), axis.text.x = element_text(size=16, angle = 90,  hjust = 0.8, vjust = 0.5), axis.ticks.x = element_blank(),
         legend.title = element_text(size=24), legend.text = element_text(size=20), strip.text = element_text(size=14), plot.tag.position = "top", plot.tag = element_text(size = 14))
 
-##############################################
+##
+## Extending the modelling to get 100 year predictions
+##
 
-# generate the fire
+# generate the first 27 years
 scenario_27 <- transitions_ot(scenarios, time=27)
 
+# generate the next 73 years using the BAU tt
 iter=0
-
 for (i in scenario_27){
   iter=iter+1
   for (j in 1:73){
@@ -104,18 +124,12 @@ for (i in scenario_27){
   scenario_27[[iter]] <- i
 }
 
+# generate the proportions of species for all scenarios using the t=100 tts
 predictions_100 <- predict_movements(scenarios = scenario_27, time = 1)
 predictions_100$scenario <- scenario_names
 
 # differences from t=0
-predict100_diff <- predictions_100
-predict100_diff[,1:6] <- apply(predict100_diff[,1:6], 2, function(x) as.numeric(x))
-predict100_diff[,1:6] <- t(apply(predict100_diff[,1:6], 1, function(x) x-props))
-
-predict100_diff <- predict100_diff[,c(1:6,8)]
-predict100_diff <- gather(predict100_diff, "category", "PEX", -scenario)
-predict100_diff$category <- factor(predict100_diff$category, levels = c("LC", "NT", "VU", "EN", "CR", "EX"))
-
+predict100_diff <- calc_diff(predictions_100, props = props)
 
 # plot it
 ggplot(data=predict100_diff, aes(x = scenario, y = PEX, fill = category)) +
@@ -128,13 +142,32 @@ ggplot(data=predict100_diff, aes(x = scenario, y = PEX, fill = category)) +
         axis.text.y = element_text(size=16) , axis.title = element_text(size=16), axis.text.x = element_text(size=16, angle = 90,  hjust = 0.8, vjust = 0.5), axis.ticks.x = element_blank(),
         legend.title = element_text(size=24), legend.text = element_text(size=20), strip.text = element_text(size=14), plot.tag.position = "top", plot.tag = element_text(size = 14))
 
-###############################################
+##
+## changes if we downlist every threatened species by 2050
+##
 
-## FUNCTIONS for plotting changes over time ##
+# 27 year species category movements - all threatened species moved down a category
+props27 <- as.numeric(c(LC=18302, NT = 4439, VU= 2429, EN = 1564, CR = 0, EX = 89))
 
-###############################################
+# grab 73 year movements
+standard_73tt <- as.matrix(Standard_tt %^% 73)
 
-calc_movements <- function(props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2429, CR = 1564, EX = 89),
+# apply to each other to get species per category after 73 years
+predict_uplist <- predict_movements(props = props27, scenarios = list(standard_73tt), time = 1)
+predict_uplist[,1:6] <- apply(predict_uplist[,1:6],2,function(x) as.numeric(x))
+predict_uplist[,1:6] <- t(apply(predict_uplist[,1:6], 1, function(x) x-props))
+
+## plotting?? ##
+
+########################
+## Changes over time ##
+#######################
+
+## FUNCTIONS ##
+
+# calculates the distribution of species between categories given a starting
+# distribution and a transition table
+calc_movements <- function(props,
                            scenario){
   prop_pred <- data.frame(scenario)
   prop_pred[,7] <- data.frame(nums = props)
@@ -145,10 +178,14 @@ calc_movements <- function(props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2429
   return(prop_pred)
 }
 
-movementsOT <- function(conservation_scenario = Standard_tt, time = 100){ 
+# uses a transition table and forecasts how a given distribution of species
+# across categories changes over a given time. 
+# requires calc_movements
+movementsOT <- function(conservation_scenario = Standard_tt, time = 100, 
+                        props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2429, CR = 1564, EX = 89)){ 
   scenario <- conservation_scenario
   predOT <- data.frame(matrix(ncol=6, nrow=0))
-  predOT <- rbind(predOT, calc_movements(scenario = conservation_scenario))
+  predOT <- rbind(predOT, calc_movements(scenario = conservation_scenario, props = props))
   names(predOT) <- Categories
   # names(predOT) <- Categories
   for(i in 1:time){
@@ -158,6 +195,7 @@ movementsOT <- function(conservation_scenario = Standard_tt, time = 100){
   return(predOT)
 }
 
+# formats the output from movementsOT ready for plotting
 format_for_plot <- function(predOT){
   predOT <- rownames_to_column(predOT, "Time")
   predOT$Time <- as.numeric(predOT$Time)-1
@@ -165,11 +203,11 @@ format_for_plot <- function(predOT){
   return(predOT)
 }
 
+# plots the output from format_for_plot
 Plot_TT <- function(transitionTable){
     standard100<-movementsOT(conservation_scenario = transitionTable)
     standard100 <- format_for_plot(standard100)
     standard100$ThreatLevel <- factor(standard100$ThreatLevel, levels = c("EX", "CR", "EN", "VU", "NT", "LC"))
-    
     p <- ggplot(data = standard100, aes(x = Time, y = Value, colour = ThreatLevel)) + scale_color_manual(values = c("black","darkred", "darkorange", "gold", "darkcyan", "lightblue"))
     p <- p + geom_line(size=1.2) + scale_y_continuous(breaks = seq(0,22000,5000))
     #p <- p + geom_ribbon(aes(ymin=0, ymax=Value, alpha=0.5),fill="lightgrey", linetype = 0, show.legend = FALSE)
@@ -179,46 +217,36 @@ Plot_TT <- function(transitionTable){
                    legend.position = c(0.2,0.8), legend.text = element_text(size=14), legend.title = element_text(size=16), strip.text = element_text(size=14))
     return(p)
 }
-###############################################
-## changes if we uplist every threatened species by 2050
-##############################################
 
-# 27 year species category movements
-props27 <- as.numeric(c(LC=18302, NT = 4439, VU= 2429, EN = 1564, CR = 0, EX = 89))
 
-# grab 73 year movements
-standard_73tt <- as.matrix(Standard_tt %^% 73)
 
-# apply to each other to get species per category after 73 years
-predict_uplist <- predict_movements(props = props27, scenarios = list(standard_73tt), time = 1)
-
-predict_uplist[,1:6] <- apply(predict_uplist[,1:6],2,function(x) as.numeric(x))
-predict_uplist[,1:6] <- t(apply(predict_uplist[,1:6], 1, function(x) x-props))
-
-#######################################
-## Conservation strategy modelling ##
-####################################
+## DATA ##
 
 DoubleDownAll <- as.matrix(read.csv(file = "../Data/Transition_tables/DoubleDownlistAll.csv", row.names = 1))
 HalfUpAll <- as.matrix(read.csv(file = "../Data/Transition_tables/HalveUplistAll.csv", row.names = 1))
 
-# generate 27 year predictions
+DoubleDownThreat <- as.matrix(read.csv(file = "../Data/Transition_tables/DoubleDownlistThreatened.csv", row.names = 1))
+DoubleDownENCR <- as.matrix(read.csv(file = "../Data/Transition_tables/DoubleDownlistEN_CR.csv", row.names = 1))
+DoubleDownCR <- as.matrix(read.csv(file = "../Data/Transition_tables/DoubleDownlistCR.csv", row.names = 1))
+
+HalfUpThreat <- as.matrix(read.csv(file = "../Data/Transition_tables/HalveUplistThreatened.csv", row.names = 1))
+HalfUpENCR <- as.matrix(read.csv(file = "../Data/Transition_tables/HalveUplistEN_CR.csv", row.names = 1))
+HalfUpCR <- as.matrix(read.csv(file = "../Data/Transition_tables/HalveUplistCR.csv", row.names = 1))
+
+## WORKFLOW ##
+
+##
+## Conservation strategy modelling ##
+##
+
+# generate 100 year predictions (gets no. species)
 ConSce <- predict_movements(scenarios = list(DoubleDownAll, HalfUpAll, Standard_tt), time = 100)
 ConSce$scenario <- c("Double Downlisting", "Halve Uplisting", "Business as usual")
 
-# grab the original values
-props = c(LC = 18302, NT = 1966, VU = 2473, EN = 2429, CR = 1564, EX = 89)
-props <- as.numeric(props)
+# calculate differences
+ConSce <- calc_diff(ConSce, props = props)
 
-#grab the difference between the starting values and after 27 years
-ConSce[,1:6] = apply(ConSce[,1:6], 2, function(x) as.numeric(x))
-ConSce[,1:6] <- t(apply(ConSce[,1:6], 1, function(x) x-props))
-
-## plot those differences after x years
-ConSce <- ConSce[,c(1:6,8)]
-ConSce <- gather(ConSce, "category", "PEX", -scenario)
-ConSce$category <- factor(ConSce$category, levels = c("LC", "NT", "VU", "EN", "CR", "EX"))
-
+# plot
 ggplot(data=ConSce, aes(x = scenario, y = PEX, fill = category)) +
   geom_bar(stat = "identity", position = "dodge") + 
   geom_abline(intercept = 0) +
@@ -230,6 +258,18 @@ ggplot(data=ConSce, aes(x = scenario, y = PEX, fill = category)) +
         axis.text.y = element_text(size=16, colour = c( NA, "black")) , axis.title = element_text(size=16), axis.text.x = element_text(size=16, angle = 90,  hjust = 0.8, vjust = 0.5), axis.ticks.x = element_blank(),
         legend.title = element_text(size=24), legend.text = element_text(size=20), strip.text = element_text(size=14), plot.tag.position = "top", plot.tag = element_text(size = 14))
 
+ConSceAll <- predict_movements(scenarios = list(Standard_tt, DoubleDownAll, 
+                              HalfUpAll, DoubleDownThreat, HalfUpThreat, 
+                              DoubleDownENCR, HalfUpENCR, DoubleDownCR, 
+                              HalfUpCR), time = 26)
+
+ConSceAll$scenario <- c("Business as usual", "Double Downlisting", "Halve Uplisting", 
+                     "Double Downlisting (Threatened)", "Halve Uplisting (Threatened)", 
+                     "Double Downlisting (EN & CR)", "Halve Uplisting (EN & CR)", 
+                     "Double Downlisting (CR)", "Halve Uplisting (CR)")
+
+ConSceAll[,1:6] = apply(ConSceAll[,1:6], 2, function(x) as.numeric(x))
+ConSceAll[,1:6] <- t(apply(ConSceAll[,1:6], 1, function(x) x-props))
 
 #############
 # SANDBOX #
